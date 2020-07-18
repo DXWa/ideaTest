9728fc35ed384674913cba7463df3b0e4113d4b5
ZG <1842650350@qq.com>
2020-07-18 09:44:55 +0800

    idea的git第一提交，已自动忽略.idea等文件

---
+++ git_test/src/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ZG
+ * @Date: 2020/07/18/9:38
+ * @Des:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
