e5d9db010c82fbca149bb4578351284cfa732d5e
ZG <1842650350@qq.com>
2020-07-18 10:27:50 +0800

    idea的git第一提交，已自动忽略.idea等文件

--- git_test/src/GitTest.java
+++ git_test/src/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git测试");
         System.out.println("clone提交的测试");
+        System.out.println(11);
+        System.out.println(11);
     }
 }
